de743a0f5f236c613027821b997c5bd491fdb5e6
lijiawei <97830321+mobiliabrus@users.noreply.github.com>
2023-03-06 11:42:19 +0800

    Update util.js

--- src/components/util.js
+++ src/components/util.js
@@ -1,3 +1,11 @@
 export const htmlMinify = (html) => {
   return html.replace(/\n/g, "");
 };
+
+export const debounce = (fn, interval = 0) => {
+  let timeout;
+  return (...args) => {
+    clearTimeout(timeout);
+    timeout = setTimeout(() => fn.apply(this, args), interval);
+  };
+};
