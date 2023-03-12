add83c14dad21feec10ac2e6119f9df38d66c3c5
lijiawei <jawoo@qq.com>
2023-03-12 20:42:03 +0800

    docs

--- dist/components.umd.js
+++ dist/components.umd.js
@@ -8454,7 +8454,6 @@
     }
   };
 
-  console.log(TypographyParagraph);
   var createApp = window.Vue.createApp;
   var wrapper = document.createElement('div');
   wrapper.style.position = 'fixed';
@@ -8462,12 +8461,14 @@
   wrapper.style.left = '0';
   document.body.appendChild(wrapper);
   createApp({
-    template: htmlMinify("<div v-if=\"text\" :style=\"{ position: 'absolute', left: x + 'px', top: y + 'px', background: '#fff' }\">\n  <a-typography-paragraph :copyable=\"{ text }\" style=\"display: flex; margin: 0;\">\n    {{ text }}\n  </a-typography-paragraph>\n</div>"),
+    template: htmlMinify("<div v-if=\"text\" :style=\"{ position: 'absolute', left: x + 'px', top: y + 'px', width: width + 'px', height: height + 'px', background: '#fff' }\">\n  <a-typography-paragraph :copyable=\"{ text }\" style=\"margin: 0; text-align: center;\"></a-typography-paragraph>\n</div>"),
     data: function data() {
       return {
         text: '',
         x: 0,
-        y: 0
+        y: 0,
+        width: 0,
+        height: 0
       };
     },
     mounted: function mounted() {
@@ -8479,13 +8480,18 @@
     methods: {
       listener: function listener() {
         this.text = document.getSelection().toString();
+        console.log(document.getSelection().getRangeAt(0).getClientRects());
         var _document$getSelectio = document.getSelection().getRangeAt(0).getClientRects(),
           _document$getSelectio2 = _slicedToArray(_document$getSelectio, 1),
           _document$getSelectio3 = _document$getSelectio2[0],
           x = _document$getSelectio3.x,
-          y = _document$getSelectio3.y;
-        this.x = Math.floor(x);
-        this.y = Math.floor(y);
+          y = _document$getSelectio3.y,
+          width = _document$getSelectio3.width,
+          height = _document$getSelectio3.height;
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
       }
     },
     components: {
--- src/select.js
+++ src/select.js
@@ -1,8 +1,6 @@
 import { htmlMinify } from './components/util';
 import { TypographyParagraph } from './components/antd';
 
-console.log(TypographyParagraph);
-
 const { createApp } = window.Vue;
 
 const wrapper = document.createElement('div');
@@ -12,16 +10,16 @@ wrapper.style.left = '0';
 document.body.appendChild(wrapper);
 
 createApp({
-  template: htmlMinify(`<div v-if="text" :style="{ position: 'absolute', left: x + 'px', top: y + 'px', background: '#fff' }">
-  <a-typography-paragraph :copyable="{ text }" style="display: flex; margin: 0;">
-    {{ text }}
-  </a-typography-paragraph>
+  template: htmlMinify(`<div v-if="text" :style="{ position: 'absolute', left: x + 'px', top: y + 'px', width: width + 'px', height: height + 'px', background: '#fff' }">
+  <a-typography-paragraph :copyable="{ text }" style="margin: 0; text-align: center;"></a-typography-paragraph>
 </div>`),
   data() {
     return {
       text: '',
       x: 0,
       y: 0,
+      width: 0,
+      height: 0,
     };
   },
   mounted() {
@@ -33,9 +31,12 @@ createApp({
   methods: {
     listener() {
       this.text = document.getSelection().toString();
-      const [{ x, y }] = document.getSelection().getRangeAt(0).getClientRects();
-      this.x = Math.floor(x);
-      this.y = Math.floor(y);
+      console.log(document.getSelection().getRangeAt(0).getClientRects());
+      const [{ x, y, width, height }] = document.getSelection().getRangeAt(0).getClientRects();
+      this.x = x;
+      this.y = y;
+      this.width = width;
+      this.height = height;
     },
   },
   components: {
