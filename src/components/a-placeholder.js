65b526b5bff839e5e2353415ace3ec2094965099
lijiawei <jawoo@qq.com>
2023-12-21 19:49:06 +0800

    dist

--- src/components/a-placeholder.js
+++ src/components/a-placeholder.js
@@ -1,7 +1,7 @@
 import { htmlMinify } from "./util";
 
 const template = htmlMinify(`
-<div style="width:100%;height:160px;text-align:center;line-height:120px;background:#fafafa">
+<div style="width:100%;height:200px;text-align:center;line-height:120px;background:#fafafa">
   <slot name="default"></slot>
 </div>
 `);
