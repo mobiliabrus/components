a417dff7ad93129cc3691e0b1ccd1e70de2288bf
plus <97830321+mobiliabrus@users.noreply.github.com>
2025-10-18 00:53:35 +0800

    feat: map

---
+++ src/components/ol-map/constant.ts
@@ -0,0 +1,22 @@
+export const propsType = {
+  center: {
+    type: String,
+    default: null,
+  },
+  zoom: {
+    type: Number,
+    default: 6,
+  },
+  points: {
+    type: String,
+    default: null,
+  },
+  driving: {
+    type: String,
+    default: null,
+  },
+  walking: {
+    type: String,
+    default: null,
+  },
+};
